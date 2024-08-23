ccb770c0d8224ee14b50b8d8728e395c003543df
thisiscleverson <cleverson2020333@gmail.com>
2024-08-23 08:31:09 -0300

    new configs

--- init.el
+++ init.el
@@ -1,39 +1,101 @@
-;; Remover boas-vindas do Emacs
+;; Desativar a mensagem de boas-vindas
 (setq inhibit-startup-message t)
 
-;; Remover Menus
+;; Remover barras de ferramentas e menus
 (tool-bar-mode -1)
-;(menu-bar-mode -1)
+;;(menu-bar-mode -1)  ;; Descomente esta linha se você quiser remover a barra de menus
 
-;; Numeros nas linhas
+;; Exibir números nas linhas
 (global-display-line-numbers-mode t)
 
-;; MELPA
+;; Desativar arquivos de backup (~) e auto-save (#)
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+;; resize easy
+;(global-set-key (kbd "C-<right>") 'shrink-window-horizontally)
+;(global-set-key (kbd "C-<left>") 'enlarge-window-horizontally)
+;(global-set-key (kbd "C-<up>") 'enlarge-window)
+;(global-set-key (kbd "C-<down>") 'shrink-window)
+
+;; Configuração do repositório MELPA
 (require 'package)
-(add-to-list 'package-archives '("MELPA" . "http://melpa.org/packages/"))
-
-(add-to-list 'load-path "~/.emacs.d/neotree")
-(require 'neotree)
-(global-set-key [(control ?b)] 'neotree-toggle)
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-
-(unless (package-installed-p 'gruvbox-theme)
-  (package-refresh-contents)
-  (package-install 'gruvbox-theme))
-
+(add-to-list 'package-archives '("MELPA" . "https://melpa.org/packages/"))
 (package-initialize)
 
+;; Instalar e configurar o `use-package`
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(use-package try
-  :ensure t)
+(require 'use-package)
 
+;; Tema Gruvbox
+(use-package gruvbox-theme
+  :ensure t
+  :config
+  (load-theme 'gruvbox t))
+
+;; Configuração de atalhos de teclado com `ergoemacs-mode`
+(use-package ergoemacs-mode
+  :ensure t
+  :config
+  (setq ergoemacs-theme nil)
+  (setq ergoemacs-keyboard-layout "us")
+  (ergoemacs-mode 1))
+
+;; Instalar e configurar Jedi com `use-package`
+(use-package jedi
+  :ensure t
+  :config
+  (add-hook 'python-mode-hook 'jedi:setup)
+  (setq jedi:complete-on-dot t))
+
+;; Barra lateral Neotree com ícones
+(use-package neotree
+  :ensure t
+  :config
+  (global-set-key (kbd "C-S-b") 'neotree-toggle)
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
+
+;; Linter para Python com Flycheck
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;; Which-key para dicas de teclas
 (use-package which-key
   :ensure t
   :config (which-key-mode))
 
+;; Instalar ícones para o modo gráfico
+(use-package all-the-icons
+  :if (display-graphic-p))
+
+;; Use-package para tentar pacotes
+(use-package try
+  :ensure t)
+
+;; Configuração do auto-complete para Python
+(use-package auto-complete
+  :ensure t
+  :config
+  (ac-config-default)
+  (setq ac-sources
+        '(ac-source-words-in-same-mode-buffers
+          ac-source-words-in-buffer
+          ac-source-yasnippet))
+  (add-hook 'python-mode-hook
+            (lambda ()
+              (auto-complete-mode 1)
+              ;; Verifique se ac-source-rope é um fonte válida ou substitua por alternativas
+              (set (make-local-variable 'ac-sources)
+                   (append ac-sources '(ac-source-yasnippet)))
+              (setq ac-find-function 'ac-python-find)
+              (setq ac-candidate-function 'ac-python-candidate)
+              (setq ac-auto-start nil))))
+
+;; auto-complete
 (use-package auto-complete
   :ensure t
   :init
@@ -41,29 +103,17 @@
     (ac-config-default)
     (global-auto-complete-mode t)))
 
-;; all-the-icons
-(use-package all-the-icons
-  :if (display-graphic-p))
 
-(use-package ergoemacs-mode
+
+;; shell-pop
+(use-package shell-pop
   :ensure t
-  :config
-  (progn
-    (setq ergoemacs-theme nil)
-    (setq ergoemacs-keyboard-layout "us")
-    (ergoemacs-mode 1)))
-
-;; Colocar o tema no neotree
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-
-;; definir tema
-(require 'gruvbox-theme)
-(load-theme 'gruvbox t)
-
-
-;; Python autocomplete-jedi
-(add-hook 'python-mode-hook 'jedi:ac-setup)
-(setq jedi:complete-on-dot t)
+  :custom
+  (shell-pop-shell-type '("ansi-term" "*shell-pop-ansi-term*" (lambda () (ansi-term shell-pop-term-shell))))
+  (shell-pop-term-shell "/bin/zsh")
+  (shell-pop-universal-key "C-t") ;; platformio-ide-terminal key
+  (shell-pop-window-height 30)
+  (shell-pop-window-position "bottom"))
 
 
 (custom-set-variables
@@ -71,9 +121,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" "d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" default))
- '(package-selected-packages '(which-key try)))
+ '(package-selected-packages
+   '(shell-pop auto-complete-chunk yasnippet which-key try neotree markdown-mode gruvbox-theme flycheck-inline ergoemacs-mode company auto-complete all-the-icons ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
